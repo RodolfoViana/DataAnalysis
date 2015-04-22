8b57397e4b0f71d0b0556b15356715199aa4342f
rodolfoviana <rodolfodelimaviana@gmail.com>
2015-04-21 22:39:55 -0300

    Minhas perguntas

--- Assignment1/AnaliseAnoAtual.R
+++ Assignment1/AnaliseAnoAtual.R
@@ -11,10 +11,15 @@ file <- read.csv("Projetos/DataAnalysis/Assignment1/AnoAtual.csv")
 groupedByNumSubCota <- group_by(file, numSubCota)
 
 # Selecionando apenas a coluna de item e descriçao
-itemDescricao <- select(file, numSubCota, txtDescricao)
+
+itemDescricao <- select(file, txtDescricao, numSubCota)
 itemDescricao <- itemDescricao[!duplicated(itemDescricao),]
-itemDescricao <- itemDescricao[order(itemDescricao$numSubCota), ]
-colnames(itemDescricao) <- c("Tipo", "Despesa")
+itemDescricaoOrder <- itemDescricao[order(itemDescricao$numSubCota), ]
+colnames(itemDescricaoOrder) <- c("Tipo", "Despesa")
+itemDescricaoOrder$Tipo <- as.character(itemDescricaoOrder$Tipo)
+itemDescricaoOrder$Despesa <- as.character(itemDescricaoOrder$Despesa)
+itemDescricaoOrder
+
 
 # Somando o valor liquido agrupado pelo item
 valorLiquido <- summarise(groupedByNumSubCota, sum(vlrLiquido))
@@ -22,16 +27,15 @@ colnames(valorLiquido) <- c("Tipo", "Descricao")
 valorLiquido <- valorLiquido[order(valorLiquido$Descricao), ]
 valorLiquido$Tipo <- as.character(valorLiquido$Tipo)
 
-# Plotando no mundo da imaginaçao
+# Plotando os valores liquidos
 ggplot(valorLiquido, aes(x=reorder(Tipo, -Descricao), y=Descricao)) + geom_bar(stat = "identity") + labs(x='Tipo da Despesa', y='Valor dos Gastos em Reais') + geom_bar(stat='identity',data=subset(valorLiquido,valorLiquido$Tipo==5),fill='blue') + scale_y_continuous(breaks = c(0,2000000,4000000,6000000))
-ggplot(valorLiquido, aes(x=reorder(Tipo, -Descricao), y=Descricao)) + geom_bar(stat = "identity") + labs(x='Tipo da Despesa', y='Valor dos Gastos em Reais') + geom_bar(stat='identity',data=subset(valorLiquido,valorLiquido$Tipo==5),fill='blue') + scale_y_continuous(trans = "log")
+# ggplot(valorLiquido, aes(x=reorder(Tipo, -Descricao), y=Descricao)) + geom_bar(stat = "identity") + labs(x='Tipo da Despesa', y='Valor dos Gastos em Reais') + geom_bar(stat='identity',data=subset(valorLiquido,valorLiquido$Tipo==5),fill='blue') + scale_y_continuous(trans = "log")
 
-valorLiquido
 
 # Quais tipos de despesas têm despesas que mais variam, e que são mais desiguais? 
 desvio <- summarise(groupedByNumSubCota, sd(vlrLiquido))
-colnames(desvio) <- c("Tipo", "Desvio")
+#colnames(desvio) <- c("Tipo", "Desvio")
 desvio$Tipo <- as.character(desvio$Tipo)
 ggplot(desvio, aes(x=reorder(Tipo, -Desvio), y=Desvio)) + geom_bar(stat = "identity") + labs(x='Tipo da Despesa', y='Variação em reais') 
 
-+ scale_y_continuous(trans = "log")
+# + scale_y_continuous(trans = "log")
---
+++ Assignment1/MinhasPerguntas.R
@@ -0,0 +1,7 @@
+library(dplyr)
+library(ggplot2)
+
+
+# Qual é empresa aérea preferida pelos nossos parlamentares ?
+
+file <- read.csv("Projetos/DataAnalysis/Assignment1/AnoAtual.csv")

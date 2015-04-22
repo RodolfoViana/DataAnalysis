81327a207df6823bcb76da711a82f7e888b317f3
rodolfoviana <rodolfodelimaviana@gmail.com>
2015-04-21 23:32:51 -0300

    First question

--- Assignment1/MinhasPerguntas.R
+++ Assignment1/MinhasPerguntas.R
@@ -1,7 +1,24 @@
 library(dplyr)
 library(ggplot2)
 
+# Descobrir se os parlamentares compram para outras pessoas
+txtPassageiro
+
+# Diferença entre o 9 e o 999 ? 
+# Despesas no exterior ?
 
 # Qual é empresa aérea preferida pelos nossos parlamentares ?
 
 file <- read.csv("Projetos/DataAnalysis/Assignment1/AnoAtual.csv")
+fileBeneficiario <- select(file, txtDescricao, numSubCota, txtCNPJCPF, txtDescricaoEspecificacao, txtDescricao, txtBeneficiario)
+
+verificar o cnpj dessas empresas
+itemDescricao <- filter(fileBeneficiario, numSubCota == 999)
+
+numSubCota9 <- filter(fileBeneficiario, numSubCota == 9)
+
+test <- filter(fileBeneficiario, numSubCota == 9)
+tabelaCNPJAviao <- select(test, txtCNPJCPF, txtBeneficiario)
+tabelaCNPJAviaoUnique <- unique(tabelaCNPJAviao)
+cnpjcpg <- unique(test$txtCNPJCPF)
+
